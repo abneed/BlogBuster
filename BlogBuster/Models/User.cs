cd081008503bf56d82b1dbb65cbffecb18052284
Abneed <memoboy262@hotmail.com>
2018-12-09 15:39:16 -0600

    Implementa modelo, controlador y vista para User

---
+++ BlogBuster/Models/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace BlogBuster.Models
+{
+    public class User
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [StringLength(254)]
+        [Display(Name = "Email address")]
+        [Required(ErrorMessage = "The email address is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "The password is required")]
+        [StringLength(254)]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public Gender Gender { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
