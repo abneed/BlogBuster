a122750f38cb478d3fc63070e914839e79c067da
Abneed <memoboy262@hotmail.com>
2018-12-09 13:32:01 -0600

    Diseña vistas parciales "Header" y "Footer", y diseño de la pagina.

--- BlogBuster/Controllers/HomeController.cs
+++ BlogBuster/Controllers/HomeController.cs
@@ -15,8 +15,6 @@ namespace BlogBuster.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
